c9f61d91bceb7017c2261ff4fe162e07a1a6d53f
kevin <kevin0181@naver.com>
2021-08-12 20:30:51 +0900

    make controller, index page

---
+++ src/main/java/soso/sosoproject/controller/PageController.java
@@ -0,0 +1,14 @@
+package soso.sosoproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/main/java/soso/sosoproject/controller/SecurityController.java
@@ -0,0 +1,25 @@
+package soso.sosoproject.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityController extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index","/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/index")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
