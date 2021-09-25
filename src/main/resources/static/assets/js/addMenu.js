b89cd10a214c2eabf9eba6c796fffbe556fe22fd
kevin <kevin0181@naver.com>
2021-09-25 23:05:35 +0900

    2021.09.25 change modal

--- src/main/resources/static/assets/js/addMenu.js
+++ src/main/resources/static/assets/js/addMenu.js
@@ -3,14 +3,19 @@ var category_seq;
 
 function addCategory() {
     var category_form = $("#category_form").serialize();
-    $.ajax({
-        type: "GET",
-        url: "/menu/add-category",
-        dataType: "json",
-        data: category_form
-    });
-    alert("카테고리가 추가되었습니다.");
-    location.href = "/admin/add-menu";
+    if (!$('#category_name').val()) {
+        alert("빈칸을 입력해주세요.");
+        return false;
+    } else {
+        $.ajax({
+            type: "GET",
+            url: "/menu/add-category",
+            dataType: "json",
+            data: category_form
+        });
+        alert("카테고리가 추가되었습니다.");
+        location.href = "/admin/add-menu";
+    }
 }
 
 
@@ -58,17 +63,21 @@ function addMenu() {
     var menu_form = $("#menu_id_form");
     var formData = new FormData(menu_form[0]);
 
-    $.ajax({
-        type: "POST",
-        url: "/menu/add-menu",
-        dataType: "json",
-        processData: false,
-        contentType: false,
-        data: formData
-    });
-
-    alert("메뉴가 추가되었습니다.");
-    location.href = "/admin/add-menu";
+    if (!$('#menu_name').val() || !$('#menu_price').val()) {
+        alert("빈칸을 입력해주세요.");
+        return false;
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/menu/add-menu",
+            dataType: "json",
+            processData: false,
+            contentType: false,
+            data: formData
+        });
+        alert("메뉴가 추가되었습니다.");
+        location.href = "/admin/add-menu";
+    }
 }
 
 //메뉴 삭제
@@ -77,15 +86,28 @@ function deleteMenu() {
     $("input:checkbox[name='menuCheck']:checked").each(function () {
         menuCheck.push($(this).val());     // 체크된 것만 값을 뽑아서 배열에 push
     });
-    $.ajax({
-        type: "POST",
-        url: "/menu/delete-menu",
-        dataType: "json",
-        data: {
-            "menuCheck": menuCheck,
-            "condition": "delete"
-        }
-    });
-    alert("메뉴가 삭제되었습니다.");
-    location.href = "/admin/add-menu";
+    if (isEmptyArr(menuCheck)) {
+        alert("삭제할 메뉴를 체크해주세요.");
+        return false;
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/menu/delete-menu",
+            dataType: "json",
+            data: {
+                "menuCheck": menuCheck,
+                "condition": "delete"
+            }
+        });
+        alert("메뉴가 삭제되었습니다.");
+        location.href = "/admin/add-menu";
+    }
+}
+
+//배열 빈값 체크
+function isEmptyArr(arr) {
+    if (Array.isArray(arr) && arr.length === 0) {
+        return true;
+    }
+    return false;
 }
