7f435657f14ac57c80329c9d417b107ef410411b
kevin <kevin0181@naver.com>
2021-10-01 19:31:15 +0900

    start memberList and change menu List plz

--- src/main/java/soso/sosoproject/controller/admin/AdminPageController.java
+++ src/main/java/soso/sosoproject/controller/admin/AdminPageController.java
@@ -21,6 +21,7 @@ public class AdminPageController {
     @Autowired
     private MenuService menuService;
 
+    //인덱스
     @GetMapping("index")
     public String index(@RequestParam(value = "className", required = false) String className, Model model) {
 
@@ -33,63 +34,10 @@ public class AdminPageController {
         return "admin/admin-index";
     }
 
-    //component
-    @GetMapping("component/alert")
-    public String alert() {
-        return "admin/component-alert";
-    }
-
-    @GetMapping("component/button")
-    public String button() {
-        return "admin/component-button";
-    }
-
-    @GetMapping("/component/pagination")
-    public String pagination() {
-        return "admin/component-pagination";
-    }
-
-    //extra-component
-    @GetMapping("/extracomponent/sweetalert")
-    public String sweetalert() {
-        return "admin/extra-component-sweetalert";
-    }
-
-    @GetMapping("/extracomponent/toastify")
-    public String toastify() {
-        return "admin/extra-component-toastify";
-    }
-
-
-    //form
-    @GetMapping("/form/input")
-    public String input() {
-        return "admin/form-element-input";
-    }
-
-    @GetMapping("/form/input-group")
-    public String inputGroup() {
-        return "admin/form-element-input-group";
-    }
-
-    @GetMapping("/form/select")
-    public String select() {
-        return "admin/form-element-select";
-    }
-
-    @GetMapping("/form/radio")
-    public String radio() {
-        return "admin/form-element-radio";
-    }
-
-    @GetMapping("/form/checkbox")
-    public String checkbox() {
-        return "admin/form-element-checkbox";
-    }
-
-    @GetMapping("/form/textarea")
-    public String textarea() {
-        return "admin/form-element-textarea";
+    //로그인
+    @GetMapping("login")
+    public String adminLogin() {
+        return "admin/admin-login";
     }
 
     //메뉴추가
@@ -162,53 +110,14 @@ public class AdminPageController {
     }
 
 
-    @GetMapping("/form/editor")
-    public String editor() {
-        return "admin/form-editor-summernote";
-    }
+    @GetMapping("MemberList")
+    public String memberList(@RequestParam(value = "className", required = false) String className, Model model) {
 
-
-    //table
-    @GetMapping("/table")
-    public String table() {
-        return "admin/table";
-    }
-
-    @GetMapping("/datatable")
-    public String datatable() {
-        return "admin/table-datatable";
-    }
-
-    //widget
-    @GetMapping("/widgets/chatbox")
-    public String chatbox() {
-        return "admin/ui-widgets-chatbox";
-    }
-
-    @GetMapping("/widgets/todolist")
-    public String toDoList() {
-        return "admin/ui-widgets-todolist";
-    }
-
-    @GetMapping("/fileupload")
-    public String fileUpLoad() {
-        return "admin/ui-file-uploader";
-    }
-
-
-    //auth
-    @GetMapping("/login")
-    public String authLogin() {
-        return "admin/admin-login";
-    }
-
-    @GetMapping("/auth/register")
-    public String authRegister() {
-        return "admin/auth-register";
-    }
-
-    @GetMapping("/auth/password")
-    public String authPassword() {
-        return "admin/auth-forgot-password";
+        //페이지 active 구분
+        if (className == null) {
+            className = "MemberList";
+        }
+        model.addAttribute("className", className);
+        return "admin/MemberList";
     }
 }
