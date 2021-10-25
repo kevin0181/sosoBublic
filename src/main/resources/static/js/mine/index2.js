945b25a793f0893e7a9e5f0599fc870713cf81ee
kevin <kevin0181@naver.com>
2021-10-25 21:02:51 +0900

    index order menu change css

--- src/main/resources/static/js/mine/index2.js
+++ src/main/resources/static/js/mine/index2.js
@@ -9,9 +9,9 @@ function menuClick(menuSq, menuName) {
     if ($('#menu' + menuSq).hasClass("click_white") === true) {
         $('#menu' + menuSq).removeClass("click_white");
         $('#menu' + menuSq).addClass("click_green");
-        $('#menuForm').append("<div class='col-md-4 menuColDiv" + menuSq + "' style='display: inline-block'>" + "<input type='hidden' id='menuInput" + menuSq + "' name='menuSq' value=" + menuSq + ">" +
-            "<p name='menuName'id='menuName" + menuSq + "' style='display: inline-block;font-size: 13px;font-weight: 300;' data-value=" + menuName + ">" + menuName + "</p>" +
-            "<input type='number'id='menuNumber" + menuSq + "' min='0' style='border-color: #eeeeee;padding: 0.575rem 0.75rem;color: #999999;border: 1px solid #ced4da;' class='numberClass' name='menuSize' value='1'>"
+        $('#menuForm').append("<div class='menuColDiv" + menuSq + " row' style='text-align: left'>" + "<input type='hidden' id='menuInput" + menuSq + "' name='menuSq' value=" + menuSq + ">" +
+            "<p name='menuName'id='menuName" + menuSq + "' style='font-size: 14px;padding-top: 10px;max-width: 40%;' class='col' data-value=" + menuName + ">" + menuName + "</p>" +
+            "<input type='number'id='menuNumber" + menuSq + "' min='0' class='form-control col' style='max-width: 40%;' name='menuSize' value='1'>"
             + "</div>");
     } else if ($('#menu' + menuSq).hasClass("click_green") === true) {
         $('#menu' + menuSq).removeClass("click_green");
