53705b1be44d38c02cbffb226dec0d61be42f6f5
kevin <kevin0181@naver.com>
2021-11-13 14:57:22 +0900

    소셜로그인 오류 해결

--- src/main/java/soso/sosoproject/controller/oauth2/Oauth2Controller.java
+++ src/main/java/soso/sosoproject/controller/oauth2/Oauth2Controller.java
@@ -7,12 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import soso.sosoproject.dto.*;
+import soso.sosoproject.message.AccountMessage;
 import soso.sosoproject.service.admin.menu.MenuService;
 import soso.sosoproject.service.oauth2.Oauth2DataService;
 import soso.sosoproject.service.user.UserBlogService;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -39,31 +41,35 @@ public class Oauth2Controller {
 
         oauth2DataService.saveOauth2(oauth2DTO);
 
-        session.setAttribute("memberEMail", oauth2DTO.getMemberEmail());
-        session.setAttribute("memberName", oauth2DTO.getMemberName());
-
-        MemberDTO memberDTO = oauth2DataService.findOauth2Member(oauth2DTO.getMemberEmail());
-        session.setAttribute("memberSq", memberDTO.getMember_sq());
-
-
-        //오늘의 메뉴 가져옴
-        MenuDTO todayMenu = todayMenu();
-        model.addAttribute("todayMenu", todayMenu);
-
-        //카테고리 가져옴
-        List<MenuCategoryDTO> menuCategoryDTO = getCategory();
-        model.addAttribute("category", menuCategoryDTO);
-
-        //메뉴 리스트
-        List<MenuDTO> menuDTOList = getMenuList();
-        model.addAttribute("menu", menuDTOList);
-
-        //블로그 리스트
-        Page<BlogDTO> blogDTOPageable = userBlogService.getIndexBlogPage(0);
-        model.addAttribute("blogDTO", blogDTOPageable);
+//        session.setAttribute("memberEMail", oauth2DTO.getMemberEmail());
+//        session.setAttribute("memberName", oauth2DTO.getMemberName());
+//
+//        MemberDTO memberDTO = oauth2DataService.findOauth2Member(oauth2DTO.getMemberEmail());
+//        session.setAttribute("memberSq", memberDTO.getMember_sq());
+//        Iterator<RoleDTO> roleDTOInteger = memberDTO.getRole().iterator();
+//        while (roleDTOInteger.hasNext()) {
+//            session.setAttribute("memberRole", roleDTOInteger.next());
+//        }
 
 
-        return "user/index";
+//        //오늘의 메뉴 가져옴
+//        MenuDTO todayMenu = todayMenu();
+//        model.addAttribute("todayMenu", todayMenu);
+//
+//        //카테고리 가져옴
+//        List<MenuCategoryDTO> menuCategoryDTO = getCategory();
+//        model.addAttribute("category", menuCategoryDTO);
+//
+//        //메뉴 리스트
+//        List<MenuDTO> menuDTOList = getMenuList();
+//        model.addAttribute("menu", menuDTOList);
+//
+//        //블로그 리스트
+//        Page<BlogDTO> blogDTOPageable = userBlogService.getIndexBlogPage(0);
+//        model.addAttribute("blogDTO", blogDTOPageable);
+        model.addAttribute("data", new AccountMessage("회원가입이 완료되었습니다. 다시 로그인해주세요.", "/user/account/login"));
+        return "/message/account-message";
+
     }
 
     //index 페이지 오늘의 메뉴 가지고 오는 함수
