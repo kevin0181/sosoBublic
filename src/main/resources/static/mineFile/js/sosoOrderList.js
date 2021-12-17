eaee8e727183b93399cf0761e5d12819f2114d39
youngbeen you <kevin0181@naver.com>
2021-12-17 17:24:06 +0900

    소소 주문 전체 리스트 취소 시 리다이렉트 변경

--- src/main/resources/static/mineFile/js/sosoOrderList.js
+++ src/main/resources/static/mineFile/js/sosoOrderList.js
@@ -125,3 +125,67 @@ function sosoOrderFail(memberSq, ordersMerchantUid, ordersImpUid, orders_id) {
         }
     }
 }
+
+
+function sosoOrderFailAll(memberSq, ordersMerchantUid, ordersImpUid, orders_id) {
+    if (ordersMerchantUid != null && ordersImpUid != null) {
+        //일반 주문
+        if (confirm("주문을 추소하시겠습니까?") == true) {
+            //주문 취소
+            $.ajax({
+                type: "GET",
+                url: "/admin/orderList/sosoOrderDelete",
+                dataType: "json",
+                data: {
+                    'memberSq': memberSq,
+                    'orders_id': orders_id,
+                    'ordersMerchantUid': ordersMerchantUid,
+                    'ordersImpUid': ordersImpUid
+                },
+                success: function (result) {
+
+                    if (result) {
+                        alert("주문취소를 완료하였습니다.");
+                        location.href = "/admin/All/OrderList?className=sosoList";
+                        return false;
+                    } else {
+                        alert("주문취소에 실패하였습니다.(관리자에게 문의해주세요)");
+                        return false;
+                    }
+
+                }
+            });
+
+        } else {
+            return false;
+        }
+    } else {
+        //예약 주문
+        if (confirm("주문을 취소하시겠습니까?") == true) {
+            //주문 완료
+            $.ajax({
+                type: "GET",
+                url: "/admin/orderList/sosoOrderDelete",
+                dataType: "json",
+                data: {
+                    'memberSq': memberSq,
+                    'orders_id': orders_id,
+                },
+                success: function (result) {
+
+                    if (result) {
+                        alert("주문취소를 완료하였습니다.");
+                        location.href = "/admin/All/OrderList?className=sosoList";
+                        return false;
+                    } else {
+                        alert("주문취소에 실패하였습니다.(관리자에게 문의해주세요)");
+                        return false;
+                    }
+
+                }
+            });
+        } else {
+            return false;
+        }
+    }
+}
